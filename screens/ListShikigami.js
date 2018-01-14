74367ffd31ae37d79759c01b7a918fbc68cbf805
ThrownDice <kiwlgus1@korea.ac.kr>
2018-01-15 01:16:43 +0900

    FlatListing test

--- screens/ListShikigami.js
+++ screens/ListShikigami.js
@@ -1,12 +1,61 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
+
+const dummyData = [
+  {
+    id: 1,
+    name: '스사비',
+    rarity: 'SSR',
+    type: '공격형',
+  },
+  {
+    id: 2,
+    name: '모모카',
+    rarity: 'SR',
+    type: '회복형',
+  },
+  {
+    id: 3,
+    name: '좌부동자',
+    rarity: 'R',
+    type: '보조형',
+  }
+];
+
+class ListShikigamiItem extends React.PureComponent {
+  _onPress = () => {
+
+  };
+
+  render() {
+    return (
+      <TouchableOpacity onPress={this._onPress}>
+        <View>
+          <Text> {this.props.name} </Text>
+          <Text> {this.props.rarity} </Text>
+          <Text> {this.props.type} </Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+}
 
 const ListShikigami = ({ navigation }) => (
   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>ListShikigami Screen</Text>
-    <Button
+    {/*<Button
       onPress={() => navigation.navigate('DetailShikigami')}
       title="Go to DetailShikigami"
+    />*/}
+    <FlatList
+      data={dummyData}
+      renderItem={({item}) => <ListShikigamiItem {...item} />}
+      keyExtractor={(item, index) => item.id}
     />
   </View>
 );
