7242938c1d4848467893c6a520581e02f9c81214
ThrownDice <kiwlgus1@korea.ac.kr>
2018-01-22 00:54:55 +0900

    Workin on listing

---
+++ components/IconRarity.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+export default function IconRarity({}) {
+  return (
+
+  )
+}
--- screens/ListShikigami.js
+++ screens/ListShikigami.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {
+  StyleSheet,
   View,
   Text,
   Button,
   TouchableOpacity,
   FlatList,
+  Image,
 } from 'react-native';
+
+const thumbs = {
+  tamamo: require('../assets/shikigami/thumb_tamamo.png'),
+};
 
 const dummyData = [
   {
@@ -13,18 +19,24 @@ const dummyData = [
     name: '스사비',
     rarity: 'SSR',
     type: '공격형',
+    rate: 9.0,
+    thumb: 'tamamo',
   },
   {
     id: 2,
     name: '모모카',
     rarity: 'SR',
     type: '회복형',
+    rate: 8.5,
+    thumb: 'tamamo',
   },
   {
     id: 3,
     name: '좌부동자',
     rarity: 'R',
     type: '보조형',
+    rate: 8.0,
+    thumb: 'tamamo',
   }
 ];
 
@@ -36,22 +48,26 @@ class ListShikigamiItem extends React.PureComponent {
   render() {
     return (
       <TouchableOpacity onPress={this._onPress}>
-        <View>
+        <View style={shikigamiItemStyles.wrap}>
+          <Image source={thumbs[this.props.thumb]} />
           <Text> {this.props.name} </Text>
           <Text> {this.props.rarity} </Text>
           <Text> {this.props.type} </Text>
+          <Text> {this.props.rate} </Text>
         </View>
       </TouchableOpacity>
     )
   }
 }
 
+const shikigamiItemStyles = StyleSheet.create({
+  wrap: {
+    flexDirection: 'row',
+  }
+});
+
 const ListShikigami = ({ navigation }) => (
   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    {/*<Button
-      onPress={() => navigation.navigate('DetailShikigami')}
-      title="Go to DetailShikigami"
-    />*/}
     <FlatList
       data={dummyData}
       renderItem={({item}) => <ListShikigamiItem {...item} />}
